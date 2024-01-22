025b8648cf99248313b15eacee593924e0e6e258
Marcin Kryszewski <marcin.p.kryszewski@gmail.com>
2024-01-22 15:12:05 +0100

    refactored Machine class

--- Shopfloor/Models/Machine.cs
+++ Shopfloor/Models/Machine.cs
@@ -4,13 +4,9 @@ using System.Collections.Generic;
 
 namespace Shopfloor.Models
 {
-    public class Machine : ISearchableModel, IEquatable<Machine>
+    public partial class Machine
     {
-        private readonly List<Machine> _children = [];
-        private readonly List<MachinePart> _parts = [];
-        private Machine? _parent;
         private readonly int? _id;
-        private string _path;
         private readonly string _name;
         private readonly string _number;
         private readonly bool _isActive;
@@ -20,10 +16,6 @@ namespace Shopfloor.Models
         public string Number => _number;
         public bool IsActive => _isActive;
         public int? ParentId => _parentId;
-        public IList<Machine> Children => _children.AsReadOnly();
-        public IList<MachinePart> Parts => _parts;
-        public string Path => _path;
-        public string SearchValue => _path.Replace(@"\", "");
         public Machine(int id, string name, string number, int? parent, bool isActive)
         {
             _id = id;
@@ -42,6 +34,14 @@ namespace Shopfloor.Models
             _path = Name;
             _isActive = isActive;
         }
+    }
+    public partial class Machine
+    {
+        private readonly List<Machine> _children = [];
+        private readonly List<MachinePart> _parts = [];
+        private Machine? _parent;
+        public IList<Machine> Children => _children.AsReadOnly();
+        public IList<MachinePart> Parts => _parts;
         public void AddChild(Machine machine)
         {
             _children.Add(machine);
@@ -53,6 +53,16 @@ namespace Shopfloor.Models
             _parent = parent;
             _parentId = parent.Id;
         }
+
+    }
+    public partial class Machine : ISearchableModel
+    {
+        private string _path;
+        public string Path => _path;
+        public string SearchValue => _path.Replace(@"\", "");
+    }
+    public partial class Machine : IEquatable<Machine>
+    {
         public bool Equals(Machine? other)
         {
             if (other == null) return false;
