6a1b3117eea311ff6a526bb95cabde529bddf4e6
Marcin Kryszewski <marcin.p.kryszewski@gmail.com>
2024-01-22 15:27:18 +0100

    Add some UserTests

---
+++ Shopfloor.Tests/Models/UserTests.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Shopfloor.Models;
+
+namespace Shopfloor.Tests.Models
+{
+    public class UserTests
+    {
+        [Fact]
+        public void Id_WhenIdNotProvided_ShouldReturnNull()
+        {
+            // Arrange
+            var user = new User("testUsername");
+            // Act
+            var result = user.Id;
+            // Assert
+            result.Should().BeNull();
+        }
+        [Fact]
+        public void Id_WhenIdProvided_ShouldReturnValue()
+        {
+            // Arrange
+            var user = new User(1, "testUsername", "TestName", "TestSurname", "testImagePath", true);
+            // Act
+            var id = user.Id;
+            // Assert
+            id.Should().Be(1);
+        }
+        [Fact]
+        public void Username_WhenProvided_ShouldReturnValue()
+        {
+            // Arrange
+            var expectedUsername = "testUsername";
+            var user = new User(expectedUsername);
+
+            // Act
+            var username = user.Username;
+
+            // Assert
+            username.Should().Be(expectedUsername);
+        }
+
+        [Fact]
+        public void Username_Should_Return_Correct_Value_After_Constructor_With_Id()
+        {
+            // Arrange
+            var expectedUsername = "testUsername";
+            var user = new User(1, expectedUsername, "TestName", "TestSurname", "testImagePath", true);
+
+            // Act
+            var username = user.Username;
+
+            // Assert
+            username.Should().Be(expectedUsername);
+        }
+    }
+}
--- Shopfloor/Models/User.cs
+++ Shopfloor/Models/User.cs
@@ -1,23 +1,19 @@
 using Shopfloor.Interfaces;
-using Shopfloor.Services.Providers;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Shopfloor.Models
 {
     public class User : ISearchableModel
     {
-        private readonly HashSet<Role> _roles;
+        private readonly HashSet<Role> _roles = [];
         private readonly int? _id;
-        private readonly string _username;
-        private readonly string _name;
-        private readonly string _surname;
-        private readonly string _image;
+        private readonly string _username = string.Empty;
+        private readonly string _name = string.Empty;
+        private readonly string _surname = string.Empty;
+        private readonly string _image = _defaultImagePath;
         private bool _isActive;
-
         private const string _defaultImagePath = "pack://application:,,,/Shopfloor;component/Resources/userDefault.png";
-
         public int? Id => _id;
         public string Username => _username;
         public string Image => _image.Length > 0 ? _image : _defaultImagePath;
@@ -26,7 +22,6 @@ namespace Shopfloor.Models
         public string FullName => $"{_name} {_surname}";
         public bool IsActive => _isActive;
         public string SearchValue => _username + _name + _surname;
-
         public User(
             int id,
             string username,
@@ -39,57 +34,29 @@ namespace Shopfloor.Models
             _username = username;
             _name = name;
             _surname = surname;
-            _roles = new();
             _image = imagePath;
             _isActive = isActive;
         }
-
         public User(string username, string name, string surname, string imagePath, bool isActive)
         {
             _username = username;
             _name = name;
             _surname = surname;
-            _roles = new();
             _image = imagePath;
             _isActive = isActive;
         }
-
         public User(string username)
         {
             _username = username;
-            _name = string.Empty;
-            _surname = string.Empty;
-            _roles = new();
-            _image = string.Empty;
-            _isActive = true;
         }
-
         public void SetActive(bool isActive)
         {
             _isActive = isActive;
         }
-
-        public async Task<int> Add(UserProvider provider)
-        {
-            return await provider.Create(this);
-        }
-
-        public async Task Edit(UserProvider provider)
-        {
-            await provider.Update(this);
-        }
-
-        public async Task Delete(UserProvider provider)
-        {
-            if (Id is null) return;
-            await provider.Delete((int)Id);
-        }
-
         public void AddRole(Role role)
         {
             _roles.Add(role);
         }
-
         public bool IsAuthorized(int roleValue)
         {
             return _roles.Any(role => role.Value == roleValue);
