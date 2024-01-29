ec703e3182cc981173b5f8ec747750a73b5b470f
Marcin Kryszewski <marcin.p.kryszewski@gmail.com>
2024-01-29 11:34:00 +0100

    Add ErrandStatusStore

--- Shopfloor/Models/ErrandStatus/ErrandStatusStore.cs
+++ Shopfloor/Models/ErrandStatus/ErrandStatusStore.cs
@@ -1,7 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Shopfloor.Models.ErrandStatusModel
 {
-    public class ErrandStatusStore
+    internal sealed class ErrandStatusStore
     {
-
+        private readonly IServiceProvider _databaseServices;
+        private IEnumerable<ErrandStatus> _data = [];
+        public ErrandStatusStore(IServiceProvider databaseServices)
+        {
+            _databaseServices = databaseServices;
+        }
+        public IEnumerable<ErrandStatus> Data => _data;
+        public bool IsLoaded { get; private set; }
+        public Task Load()
+        {
+            ErrandStatusProvider provider = _databaseServices.GetRequiredService<ErrandStatusProvider>();
+            _data = provider.GetAll().Result;
+            IsLoaded = true;
+            return Task.CompletedTask;
+        }
+        public async Task Reload()
+        {
+            ErrandStatusProvider provider = _databaseServices.GetRequiredService<ErrandStatusProvider>();
+            _data = await provider.GetAll();
+        }
     }
 }
