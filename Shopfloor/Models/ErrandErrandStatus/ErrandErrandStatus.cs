f21b45034c8e91451d4f144038250d23a506967d
Marcin Kryszewski <marcin.p.kryszewski@gmail.com>
2024-01-29 11:52:10 +0100

    Extended ErrandErrandStatus to full properties

--- Shopfloor/Models/ErrandErrandStatus/ErrandErrandStatus.cs
+++ Shopfloor/Models/ErrandErrandStatus/ErrandErrandStatus.cs
@@ -6,17 +6,37 @@ namespace Shopfloor.Models.ErrandErrandStatusesModel
 {
     internal sealed class ErrandErrandStatus
     {
+        private int? _errandStatusId;
+        private int? _errandId;
+        private readonly DateTime _createDate;
+        private Errand? _errand;
+        private ErrandStatus? _errandStatus;
         public ErrandErrandStatus(int errandId, int errandStatusId, DateTime createDate)
         {
-            ErrandId = errandId;
-            ErrandStatusId = errandStatusId;
-            CreateDate = createDate;
+            _errandId = errandId;
+            _errandStatusId = errandStatusId;
+            _createDate = createDate;
         }
-
-        public int ErrandId { get; set; }
-        public Errand Errand { get; set; }
-        public int ErrandStatusId { get; set; }
-        public ErrandStatus ErrandStatus { get; set; }
-        public DateTime CreateDate { get; set; }
+        public int? ErrandId => _errandId;
+        public Errand? Errand
+        {
+            get => _errand;
+            set
+            {
+                _errand = value;
+                _errandId = value?.Id;
+            }
+        }
+        public ErrandStatus? ErrandStatus
+        {
+            get => _errandStatus;
+            set
+            {
+                _errandStatus = value;
+                _errandStatusId = value?.Id;
+            }
+        }
+        public int? ErrandStatusId => _errandStatusId;
+        public DateTime CreateDate => _createDate;
     }
 }
