471e1fa785ed34d620dda2b2c574f9a1e6e420ae
Marcin Kryszewski <marcin.p.kryszewski@gmail.com>
2024-03-07 16:03:23 +0100

    Datagrid visual bug fix

--- Shopfloor/Features/Plannist/Offers/OffersView.xaml.cs
+++ Shopfloor/Features/Plannist/Offers/OffersView.xaml.cs
@@ -1,17 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Shopfloor.Utilities;
 
 namespace Shopfloor.Features.Plannist.Offers
 {
@@ -21,5 +10,6 @@ namespace Shopfloor.Features.Plannist.Offers
         {
             this.InitializeComponent();
         }
+        public void DataGrid_TargetUpdated(object sender, DataTransferEventArgs e) => DataGridWidthRefresh.RefreshWidth(sender, e, 7);
     }
 }
