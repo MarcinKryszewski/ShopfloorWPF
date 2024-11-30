a23052fc1693f3f27e593fe5493aff3839e46c34
MarcinKryszewski <marcin.p.kryszewski@gmail.com>
2024-11-30 01:59:39 +0100

    Adding scrolling to DataGrid

--- Shopfloor/Features/ActionsList/ActionsListView.xaml.cs
+++ Shopfloor/Features/ActionsList/ActionsListView.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Input;
 using Shopfloor.Utilities;
 
 namespace Shopfloor.Features.ActionsList
@@ -11,5 +12,15 @@ namespace Shopfloor.Features.ActionsList
             this.InitializeComponent();
         }
         public void DataGrid_TargetUpdated(object sender, DataTransferEventArgs e) => DataGridWidthRefresh.RefreshWidth(sender, e, 4);
+        private void DataGrid_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var scrollViewer = OuterScrollViewer;
+
+            if (scrollViewer != null)
+            {
+                scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - (e.Delta / 3.0));
+                e.Handled = true;
+            }
+        }
     }
 }
