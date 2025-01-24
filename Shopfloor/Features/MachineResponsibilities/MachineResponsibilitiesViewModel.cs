d6aedf22e0887516b2231c815cd74234adf0b887
Marcin Kryszewski <marcin.p.kryszewski@gmail.com>
2025-01-24 16:24:43 +0100

    Machine responsibility feature

--- Shopfloor/Features/MachineResponsibilities/MachineResponsibilitiesViewModel.cs
+++ Shopfloor/Features/MachineResponsibilities/MachineResponsibilitiesViewModel.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using Shopfloor.Models.Lines;
+using Shopfloor.Models.Machines;
 using Shopfloor.Roots;
 using Shopfloor.Shared.ViewModels;
 
@@ -9,12 +12,24 @@ namespace Shopfloor.Features.MachineResponsibilities
     internal class MachineResponsibilitiesViewModel : ViewModelBase
     {
         private readonly MachinesRoot _machinesRoot;
-
-        public MachineResponsibilitiesViewModel(MachinesRoot machinesRoot)
+        private readonly IEnumerable<Line> _lines;
+        // private readonly DataRoot _data;
+        public MachineResponsibilitiesViewModel(
+            MachinesRoot machinesRoot,
+            DataRoot data)
         {
             _machinesRoot = machinesRoot;
-            Task.Run(_machinesRoot.GetData);
+            // _data = data;
+
+            // Machines.GroupDescriptions.Clear();
+            Machines.GroupDescriptions.Add(new PropertyGroupDescription(nameof(Machine.LineId)));
+
+            // Task.Run(_machinesRoot.GetData);
+            Task.Run(_machinesRoot.GetData).Wait();
+            _lines = data.GetLine().Result;
+            Machines.Refresh();
         }
         public ICollectionView Machines => CollectionViewSource.GetDefaultView(_machinesRoot.Data);
+        public ICollectionView Lines => CollectionViewSource.GetDefaultView(_lines);
     }
 }
